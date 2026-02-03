96d206c5534bdd0cc22cd6ac212592e4fdc5ff5d
Diwas Poudel <diwaspoudel.dev@gmail.com>
2026-02-03 17:32:43 -0500

    Frontend implementation

--- Program.cs
+++ Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddIdentityCore<ApplicationUser>(options =>
         options.SignIn.RequireConfirmedAccount = true;
         options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
     })
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
---
+++ Utilities/Constant.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Utilities
+{
+    public static class RoleConstant
+    {
+        public static string Admin = "Admin";
+        public static string Customer = "Customer";
+    }
+}
