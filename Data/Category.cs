dfee097b2bf71d8125e1f5cd4199ef9ad913a05c
Diwas Poudel <diwaspoudel.dev@gmail.com>
2026-02-03 11:51:41 -0500

    Creating CategoryRepository and Icategoryrespository

---
+++ Data/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorEcommerce.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name..")]
+        public string? Name { get; set; }
+    }
+}
+
--- Data/Migrations/20260203163906_Seed Category.Designer.cs
+++ Data/Migrations/20260203163906_Seed Category.Designer.cs
@@ -4,6 +4,7 @@ using BlazorEcommerce.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorEcommerce.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260203163906_Seed Category")]
+    partial class SeedCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -102,7 +105,7 @@ namespace BlazorEcommerce.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Category", (string)null);
+                    b.ToTable("Categories");
 
                     b.HasData(
                         new
